c223c9c36e581041fb6510f0cfeba4437046cb44
unknown <you@example.com>
2020-11-10 18:15:26 +0700

    add email

--- resources/views/emails/layouts/master.blade.php
+++ resources/views/emails/layouts/master.blade.php
@@ -1,5 +1,5 @@
-<div style="margin:0 auto; font-family:Helvetica, sans-serif; padding: 15px 0;background: #f3f3f3;; font-size: 1.1em">
-    <div style="background: #fff; color: #000000; max-width:615px;margin: 0 auto; padding: 15px">
+<div style="margin:0 auto; font-family:Helvetica, sans-serif; padding: 15px 0; font-size: 1.1em">
+    <div style="background: #fff; color: #000000; max-width:561px; margin: 0 auto; padding: 15px; border: solid thin #dadce0; border-radius: 8px;">
         <div>
             @include('emails.layouts.master.header')
             <div style="margin-top: 40px">
